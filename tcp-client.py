98e0b20b53d03ed910cb5766f37698033dad0853
anaunz <anaunz@gmail.com>
2019-02-25 22:52:17 +0700

    Done 2

--- tcp-client.py
+++ tcp-client.py
@@ -3,26 +3,34 @@ import sys
 
 MAX_BUF = 2048
 SERV_PORT = 50000
-
-line = input('> ')
-temp = line.split(' ')
-command = temp[0]
-if command == 'subscribe' and len(temp) == 3:
-  broker_ip = temp[1]
-  sentmsg = temp[0] + '?' + temp[2] + '?'
-elif command == 'publish' and len(temp) == 4:
-  broker_ip = temp[1]
-  sentmsg = temp[0] + '?' + temp[2] + '?' + temp[3]
-else:
-  print('Command does not exist')
-
-addr = (broker_ip, SERV_PORT)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(addr)
-s.send(sentmsg.encode('utf-8'))
+isConnected = False
 
 while True:
-  modifiedMsg = s.recv(2048)
-  print (modifiedMsg.decode('utf-8'))
+  line = input('> ')
+  temp = line.split(' ')
+  command = temp[0]
+  if temp[2][0] is not '/':
+    temp[2] = '/' + temp[2]
+  if command == 'subscribe' and len(temp) == 3:
+    broker_ip = temp[1]
+    sentmsg = temp[0] + '?' + temp[2] + '?'
+  elif command == 'publish':
+    if len(temp) > 3:
+      broker_ip = temp[1]
+      sentmsg = temp[0] + '?' + temp[2] + '?' + ' '.join(temp[3:])
+    else:
+      print('No data')
+  else:
+    print('Command does not exist')
 
+  if not isConnected:
+    addr = (broker_ip, SERV_PORT)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(addr)
+    isConnected = True
+  s.send(sentmsg.encode('utf-8'))
+
+  while True and command == 'subscribe':
+    modifiedMsg = s.recv(2048)
+    print (modifiedMsg.decode('utf-8'))
 s.close()
--- tcp-threaded-server.py
+++ tcp-threaded-server.py
@@ -4,50 +4,63 @@ from threading import Thread
 import os,sys
 
 SERV_PORT = 50000
-listOfTopic = []
 
 class sub_topic():
-  def __init__(self):
-    self.name : str = None
+  def __init__(self, name):
+    self.name = name
     self.subscribers = []
     self.data = []
+    self.child = []
+  def addSub(self, path, sckt):
+    self.subscribers.append(sckt)
+    childExists = False
+    if len(path) > 1:
+      for x in self.child:
+        if x.name == path[1]:
+          x.addSub(path[1:], sckt)
+          childExists = True
+          break
+      if not childExists:
+        child = sub_topic(path[1])
+        child.addSub(path[1:], sckt)
+        self.child.append(child)
+  def sendData(self,path,data):
+    if len(path) > 1:
+      for x in self.child:
+        if x.name == path[1]:
+          x.sendData(path[1:], data)
+    else:
+      for x in self.subscribers:
+        x.send(data.encode('utf-8'))
+  def removeData(self, path, sckt):
+    self.subscribers.remove(sckt)
+    print('Removed socket in ' + path[0])
+    if len(path) > 1:
+      for x in self.child:
+        if x.name == path[1]:
+          x.removeData(path[1:], sckt)
+
+rootOfTopics = sub_topic('root')
 
 def handle_client(s, ip):
-  while True:
-    txtin = s.recv(2048).decode('utf-8')
-    print(ip + '> ' + txtin)
-    command, topic, data = txtin.split('?')
-    if command == 'subscribe':
-      if not listOfTopic:
-        temp = sub_topic()
-        temp.name = topic
-        temp.subscribers.append(s)
-        listOfTopic.append(temp)
+  try:
+    while True:
+      txtin = s.recv(2048).decode('utf-8')
+      print(ip + '> ' + txtin)
+      command, topic, data = txtin.split('?')
+      path = topic.split('/')
+      if command == 'subscribe':
+        rootOfTopics.addSub(path, s)
         print('Created ' + topic + ' as a new topic')
+        txtout = 'You have subscribed to ' + topic
+        s.send(txtout.encode('utf-8'))
+      elif command == 'publish':
+        rootOfTopics.sendData(path,data)
       else:
-        topicExists = False
-        for x in listOfTopic:
-          if x.name == topic:
-            x.subscribers.append(s)
-            topicExists = True
-            break
-        if not topicExists:
-          temp = sub_topic()
-          temp.name = topic
-          temp.subscribers.append(s)
-          listOfTopic.append(temp)
-          print('Created ' + topic + ' as a new topic')
-      txtout = 'You have subscribed to ' + topic
-      s.send(txtout.encode('utf-8'))
-    elif command == 'publish':
-      for x in listOfTopic:
-        print(x.subscribers)
-        if x.name == topic:
-          for y in x.subscribers:
-            y.send(data.encode('utf-8'))
-    else:
-      txtout = 'Sorry, ' + command + ' does not exist'
-      s.send(txtout.encode('utf-8'))
+        txtout = 'Sorry, ' + command + ' does not exist'
+        s.send(txtout.encode('utf-8'))
+  except Exception:
+    rootOfTopics.removeData(path, s)
   s.close()
 
 def main():
@@ -73,11 +86,11 @@ def main():
   s.close()
 
 if __name__ == '__main__':
-   try:
-     main()
-   except KeyboardInterrupt:
-     print ('Interrupted..')
-     try:
-       sys.exit(0)
-     except SystemExit:
-       os._exit(0)
+  try:
+    main()
+  except KeyboardInterrupt:
+    print ('Interrupted..')
+    try:
+      sys.exit(0)
+    except SystemExit:
+      os._exit(0)
